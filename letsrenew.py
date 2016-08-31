b33dfb6b7ba1cca3ce718c0c281ed71e719862cf
mrngm <har@mrngm.com>
2016-08-31 23:22:25 +0200

    letrenew.py: added dry-run. load certificates in one place and simplify days_to_expiry()

--- letsrenew.py
+++ letsrenew.py
@@ -8,23 +8,18 @@ import argparse
 
 from datetime import datetime as dt
 from os import listdir
-from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.backends import default_backend, openssl
 from cryptography import x509
+from cryptography.x509.oid import NameOID
 
-
-def days_to_expiry(cert_file):
+def days_to_expiry(certificate):
     """Get the number of days before a certificate expires"""
+    return (certificate.not_valid_after - dt.utcnow()).days
 
-    try:
-        certificate = open(cert_file, 'rb').read()
-        cert = x509.load_pem_x509_certificate(certificate, default_backend())
-
-        now = dt.utcnow()
-
-        return (cert.not_valid_after - now).days
-
-    except OSError as error:
-        print("OSError while trying to read file " + cert_file + ": " + error)
+def print_certificate_information(certificate):
+    """Print some information about the supplied certificate"""
+    common_name = certificate.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+    print("CN: " + common_name + ", expires in " + str(days_to_expiry(certificate)) + " days.")
 
 def main():
     """Build the argument parser and run the program"""
@@ -34,18 +29,27 @@ def main():
                         dest='certdir', help=u'Directory where to look for certificates')
     parser.add_argument('--dry-run', '-n', default=False, action="store_true",
                         help=u'Only print certificate common name (CN) and days to expiry')
-    parser.add_argument('--cert-ext', metavar='CERT_EXT', dest='cert_ext',
-                        help=u'Default extension for certificate files (defaults to .crt)',
-                        default='.crt')
+    parser.add_argument('--cert-ext', metavar='CERT_EXT', dest='cert_ext', default='.crt',
+                        help=u'Default extension for certificate files (defaults to .crt)')
     parser.add_argument('--ignore-substring', metavar='STR', dest='ignore_substring',
                         help=u'Ignore files containing STR', default='')
     args = parser.parse_args()
 
-    certificates = [c for c in listdir(args.certdir) if c.endswith(args.cert_ext)]
-    if args.ignore_substring != '':
-        certificates = [c for c in certificates if not args.ignore_substring in c]
+    certdir = args.certdir if args.certdir.endswith('/') else args.certdir + '/'
 
-    print(certificates)
+    certificates = [c for c in listdir(certdir) if c.endswith(args.cert_ext)]
+    if args.ignore_substring != '':
+        certificates = [c for c in certificates if args.ignore_substring not in c]
+
+    if args.dry_run:
+        for c in certificates:
+            try:
+                cfile = open(certdir + c, 'rb').read()
+                cert = x509.load_pem_x509_certificate(cfile, default_backend())
+                print_certificate_information(cert)
+            except OSError as error:
+                print("OSError in reading " + args.certdir + c + ": " + error)
+        raise SystemExit
 
 if __name__ == '__main__':
     main()
